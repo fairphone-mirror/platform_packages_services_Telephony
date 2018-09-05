73fc81fedc0d70bab2f8453834957e2a0695e2d8
Sridhar Kasukurthi <skasuk@codeaurora.org>
2018-09-05 17:32:43 +0530

    Fix for data roaming notification
    - During SIM hot-swap use cases by the time service state changed
      is received, data connection state might still be not connected.
      Due to this data roaming notification is not shown.
    - Fix is to register for data connection attached event and
      show data disconnected notification
    
    Change-Id: I00b5d11af48446b33e020df68083a8818f9814fb
    CRs-Fixed: 2296555
    Issue: FP3-A13#56
    (cherry picked from commit 8dccd8cb73e14ad55b252098f3456f6439693233)

--- src/com/android/phone/PhoneGlobals.java
+++ src/com/android/phone/PhoneGlobals.java
@@ -41,6 +41,7 @@ import android.preference.PreferenceManager;
 import android.provider.Settings;
 import android.sysprop.TelephonyProperties;
 import android.telecom.TelecomManager;
+import android.telephony.AccessNetworkConstants;
 import android.telephony.AnomalyReporter;
 import android.telephony.CarrierConfigManager;
 import android.telephony.ServiceState;
@@ -126,6 +127,7 @@ public class PhoneGlobals extends ContextWrapper {
     private static final int EVENT_MOBILE_DATA_SETTINGS_CHANGED = 16;
     private static final int EVENT_CARRIER_CONFIG_CHANGED = 17;
     private static final int EVENT_MULTI_SIM_CONFIG_CHANGED = 18;
+    private static final int EVENT_DATA_CONNECTION_ATTACHED = 19;
 
     // The MMI codes are also used by the InCallScreen.
     public static final int MMI_INITIATE = 51;
@@ -403,6 +405,14 @@ public class PhoneGlobals extends ContextWrapper {
                                 TelephonyManager.INCLUDE_LOCATION_DATA_NONE, mHandler::post,
                                 callback);
                         mTelephonyCallbacks[phoneId] = callback;
+                    }
+                    break;
+                case EVENT_DATA_CONNECTION_ATTACHED:
+                    subId = (Integer)((AsyncResult)msg.obj).userObj;
+                    if (!mNoDataDueToRoaming
+                            && subId == mDefaultDataSubId) {
+                        if (VDBG) Log.v(LOG_TAG, "EVENT_DATA_CONNECTION_ATTACHED");
+                        updateDataRoamingStatus();
                     }
                     break;
             }
@@ -792,6 +802,9 @@ public class PhoneGlobals extends ContextWrapper {
                 // re-register as it may be a new IccCard
                 int phoneId = intent.getIntExtra(PhoneConstants.PHONE_KEY,
                         SubscriptionManager.INVALID_PHONE_INDEX);
+                int subId = intent.getIntExtra(SubscriptionManager.EXTRA_SUBSCRIPTION_INDEX,
+                        SubscriptionManager.INVALID_SIM_SLOT_INDEX);
+                String simStatus = intent.getStringExtra(IccCardConstants.INTENT_KEY_ICC_STATE);
                 if (SubscriptionManager.isValidPhoneId(phoneId)) {
                     PhoneUtils.unregisterIccStatus(mHandler, phoneId);
                     PhoneUtils.registerIccStatus(mHandler, EVENT_SIM_NETWORK_LOCKED, phoneId);
@@ -799,6 +812,17 @@ public class PhoneGlobals extends ContextWrapper {
                 String iccStatus = intent.getStringExtra(IccCardConstants.INTENT_KEY_ICC_STATE);
                 mHandler.sendMessage(mHandler.obtainMessage(EVENT_SIM_STATE_CHANGED,
                         new EventSimStateChangedBag(phoneId, iccStatus)));
+                Phone phone = PhoneFactory.getPhone(phoneId);
+                if (phone != null) {
+                    if (IccCardConstants.INTENT_VALUE_ICC_LOADED.equals(simStatus)) {
+                        phone.getServiceStateTracker().registerForDataConnectionAttached(
+                                AccessNetworkConstants.TRANSPORT_TYPE_WWAN, mHandler, EVENT_DATA_CONNECTION_ATTACHED, subId);
+                    } else if (IccCardConstants.INTENT_VALUE_ICC_ABSENT.equals(simStatus)
+                            || IccCardConstants.INTENT_VALUE_ICC_CARD_IO_ERROR.equals(simStatus)) {
+                        phone.getServiceStateTracker()
+                                .unregisterForDataConnectionAttached(AccessNetworkConstants.TRANSPORT_TYPE_WWAN, mHandler);
+                    }
+                }
             } else if (action.equals(TelephonyIntents.ACTION_RADIO_TECHNOLOGY_CHANGED)) {
                 String newPhone = intent.getStringExtra(PhoneConstants.PHONE_NAME_KEY);
                 Log.d(LOG_TAG, "Radio technology switched. Now " + newPhone + " is active.");
