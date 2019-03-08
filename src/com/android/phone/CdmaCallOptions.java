e3009010ec1fd691ade4016c3dfe23bb736dbba7
pengfeix <pengfeix@codeaurora.org>
2019-03-08 17:33:41 +0800

    FR54939: Common call setting for specific operator
    -There are two Call settings(GSM call settings and CDMA
    call settings) for calling account and enabled one of
    them per phone object.
    
    -Hides radio technology, e.g. CDMA or GSM, details from
    the settings text. For example uses "Call Settings", instead
    of "Gsm Call Settings". PhoneType is used to display the
    correct settings when user clicks on the button.
    
    config_common_callsettings_support_bool to control this feature base
    on mnc-mcc.
    
    Change-Id: I36af46f177e01e058d44fd04fd266947ae43d320
    CRs-Fixed: 2419472
    (cherry picked from commit 1f48023b61c87e17cb58d569cd9810c86404eef4)

--- src/com/android/phone/CdmaCallOptions.java
+++ src/com/android/phone/CdmaCallOptions.java
@@ -52,6 +52,8 @@ public class CdmaCallOptions extends TimeConsumingPreferenceActivity {
     private UtCallback mUtCallback;
     private ImsMmTelManager mMmTelManager;
 
+    private boolean mCommon = false;
+
     @Override
     protected void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -59,8 +61,6 @@ public class CdmaCallOptions extends TimeConsumingPreferenceActivity {
         addPreferencesFromResource(R.xml.cdma_call_privacy);
 
         SubscriptionInfoHelper subInfoHelper = new SubscriptionInfoHelper(this, getIntent());
-        subInfoHelper.setActionBarTitle(
-                getActionBar(), getResources(), R.string.labelCdmaMore_with_label);
 
         CdmaVoicePrivacySwitchPreference buttonVoicePrivacy =
                 (CdmaVoicePrivacySwitchPreference) findPreference(BUTTON_VP_KEY);
@@ -73,6 +73,10 @@ public class CdmaCallOptions extends TimeConsumingPreferenceActivity {
             subId = SubscriptionManager.getDefaultSubscriptionId();
         }
         carrierConfig = PhoneGlobals.getInstance().getCarrierConfigForSubId(subId);
+        mCommon = carrierConfig.getBoolean("config_common_callsettings_support_bool");
+        subInfoHelper.setActionBarTitle(
+                getActionBar(), getResources(),
+                mCommon ? R.string.labelCommonMore_with_label : R.string.labelCdmaMore_with_label);
         if (subInfoHelper.getPhone().getPhoneType() != PhoneConstants.PHONE_TYPE_CDMA
                 || carrierConfig.getBoolean(CarrierConfigManager.KEY_VOICE_PRIVACY_DISABLE_UI_BOOL)) {
             buttonVoicePrivacy.setEnabled(false);
