53a2d571c5d9d844e0871fe6b71888d8e96b18ad
Pengfei Xiao <pengfeix@codeaurora.org>
2019-06-04 13:08:59 +0800

    Fix IntentReceiver leaked error
    Change-Id: I26d95e1f49151c219f3abd92eb4527b85caca742
    CRs-Fixed: 2464122
    (cherry picked from commit 82d9e8f3512bd1b81b9834f3e39d99861de9577e)

--- src/com/android/phone/GsmUmtsCallBarringOptions.java
+++ src/com/android/phone/GsmUmtsCallBarringOptions.java
@@ -520,6 +520,14 @@ public class GsmUmtsCallBarringOptions extends TimeConsumingPreferenceActivity
         }
     }
 
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mReceiver != null) {
+            unregisterReceiver(mReceiver);
+        }
+    }
+
     private void initCallBarring () {
         if (mFirstResume) {
             if (mIcicle == null || mIcicle.getBoolean(SAVED_BEFORE_LOAD_COMPLETED_KEY)) {
