61a4950760971ab95074084a05da893ee1a4792a
xiaoyang.wang <xiaoyang.wang@t2mobile.com>
2021-06-08 19:29:45 +0800

    [SIM Lock]Enter SIM PIN page always display when user change SIM PIN by MMI when PIN blocked
    Issue: ALM:11199620
    Issue: FP4-556
    Issue: FP4-A14#13
    Change-Id: Ia11dcbe1621d0315da4ddc76bfaad35ae7b1c7ff
    (cherry picked from commit 0240a34f5c4595700af48e2b7c6403d31deead29)

--- src/com/android/phone/SpecialCharSequenceMgr.java
+++ src/com/android/phone/SpecialCharSequenceMgr.java
@@ -236,8 +236,7 @@ public class SpecialCharSequenceMgr {
         // TODO: The string constants here should be removed in favor
         // of some call to a static the MmiCode class that determines
         // if a dialstring is an MMI code.
-        if ((input.startsWith("**04") || input.startsWith("**05"))
-                && input.endsWith("#")) {
+        if (input.startsWith("**04") && input.endsWith("#")) {
             UserManager userManager = (UserManager) pukInputActivity
                        .getSystemService(Context.USER_SERVICE);
             if (userManager.isSystemUser()) {
