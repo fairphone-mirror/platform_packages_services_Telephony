2e9143cb78adda5742266e665b815f882ee9e9ca
Zhang Renjie <renjiezhang@t2mobile.com>
2021-11-08 14:31:05 +0800

    FP4-3223 [FPDB-305]network selecting crash fixed
    Change-Id: Id02342c9ef451f15513562cc288f497f3947b6b0

--- src/com/android/phone/PhoneGlobals.java
+++ src/com/android/phone/PhoneGlobals.java
@@ -207,7 +207,7 @@ public class PhoneGlobals extends ContextWrapper {
     private final SipReceiver mSipReceiver = new SipReceiver();
 
     private final SetSarReceiver mSarReceiver = new SetSarReceiver();
-    private final CustomizeNVReceiver mCustNVReceiver = new CustomizeNVReceiver();//Add by shaopan.tang 2021-04-28 [FP4-89]Customize SIP/XCAP UserAgent string
+    //private final CustomizeNVReceiver mCustNVReceiver = new CustomizeNVReceiver();//Add by shaopan.tang 2021-04-28 [FP4-89]Customize SIP/XCAP UserAgent string
 
     private final CarrierVvmPackageInstalledReceiver mCarrierVvmPackageInstalledReceiver =
             new CarrierVvmPackageInstalledReceiver();
@@ -490,10 +490,10 @@ public class PhoneGlobals extends ContextWrapper {
         registerReceiver(mSarReceiver, sarIntentFilter);
 
         //[FEATURE]-Add-Begin by shaopan.tang 2021-04-28 [FP4-89]Customize SIP/XCAP UserAgent string
-        mCustNVReceiver.init(this);
+        /*mCustNVReceiver.init(this);
         IntentFilter CustNVIntentFilter = new IntentFilter(Intent.ACTION_BOOT_COMPLETED);
         CustNVIntentFilter.addAction(TelephonyIntents.ACTION_SIM_STATE_CHANGED);
-        registerReceiver(mCustNVReceiver, CustNVIntentFilter);
+        registerReceiver(mCustNVReceiver, CustNVIntentFilter);*/
         //[FEATURE]-Add-End by shaopan.tang
 
     }
