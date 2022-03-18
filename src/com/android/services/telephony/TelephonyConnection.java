46a3d42f54967965119e66f8ce4d7868264c5a35
Renjie Zhang <renjiezhang@t2mobile.com>
2022-03-18 14:50:20 +0800

    FP4-3592 [FP4 Dev-508]SVFT NL VF: NW name shown twice on WiFi call UI
    Change-Id: Ib1e67c0e143fbefd46e8b516822da023767edcfc

--- src/com/android/services/telephony/TelephonyConnection.java
+++ src/com/android/services/telephony/TelephonyConnection.java
@@ -2728,14 +2728,17 @@ abstract class TelephonyConnection extends Connection implements Holdable,
             Context context = getPhone().getContext();
 
             // add by T2M.dengxiangyu for FP4-61 2021-04-14 begin
+            // add by T2M.zhangrenjie for FP4-3592 2022-03-17 begin
             String label = getResourceString(labelId) + displaySubId;
             PersistableBundle carrierconfig = getCarrierConfig();
-            if (carrierconfig != null) {
+            if (carrierconfig != null && !isValidRingingCall()) {
                 String label_from_carrier = carrierconfig.getString(CarrierConfigManager.KEY_WIFI_CALLING_DISPLAY);
                 if (label_from_carrier != null) {
-                    label = label_from_carrier + displaySubId;
+                    label = label_from_carrier;
+                    Log.i(this, "updateStatusHints: label_from_carrier = " + label);
                 }
             }
+            // add by T2M.zhangrenjie for FP4-3592 2022-03-17 end
             // add by T2M.dengxiangyu for FP4-61 2021-04-14 end
 
             setTelephonyStatusHints(new StatusHints(
