928019e6316d4ddd7262b503bd1b2f83a2dcdabe
android-t2 <xiaocong.gu@t2mobile.com>
2022-04-22 16:00:50 +0800

    Revert "[ALM:11112302] [FP4-89]Customize xcap user agent"
    This reverts commit 008c8334fc2241a24af4afc3c7a41e9761d12ef9.

--- src/com/android/phone/CustomizeNVReceiver.java
+++ src/com/android/phone/CustomizeNVReceiver.java
@@ -13,7 +13,6 @@ import android.text.TextUtils;
 import android.util.Log;
 
 import com.android.internal.telephony.TelephonyIntents;
-import com.android.internal.telephony.IccCardConstants;
 import com.qualcomm.qcrilhook.QcRilHookCallback;
 import com.qualcomm.sysrilcmd.SysRilCmd;
 import com.qualcomm.sysrilcmd.ISysRilCmd;
