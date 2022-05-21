a18ae0718b58b1285fad58e86cde56b21629c237
Renjie Zhang <renjiezhang@t2mobile.com>
2022-05-21 14:12:13 +0800

    FP4S-61 [FP] Remove or hide redundant call settings from the menu.
    Change-Id: I25251c9042a6028b20ec64bc78f2a47fba182628

--- src/com/android/phone/settings/PhoneAccountSettingsFragment.java
+++ src/com/android/phone/settings/PhoneAccountSettingsFragment.java
@@ -383,13 +383,14 @@ public class PhoneAccountSettingsFragment extends PreferenceFragment
                 mAccountList.removePreference(mAllCallingAccounts);
                 mMakeAndReceiveCallsCategory.removePreference(mDefaultOutgoingAccount);
             }
-
-            if (isXdivertAvailable) {
+                /* add by T2M.zhangrenjie for FP4-1958 begin */
+            /*if (isXdivertAvailable) {
                 if (mSmartDivertPref != null) {
                     Log.d(LOG_TAG, "Add smart divert preference");
                     mAccountList.addPreference(mSmartDivertPref);
                 }
-            }
+            }*/
+                /* add by T2M.zhangrenjie for FP4-1958 end */
         }
     }
 
