6a690ec9a2a4641455fbb34195b088ed1c031ee1
Haibiao.Lu <Haibiao.lu@t2mobile.com>
2022-07-21 14:18:10 +0800

    [fp4s-407] [DSD][DSD]The default browser home page of MCC 208 MNC 01 should not be Google
    Change-Id: I5cdfaead1848b6114e5059ff7e7dd62a43ed83a6

--- src/com/android/phone/CarrierConfigLoader.java
+++ src/com/android/phone/CarrierConfigLoader.java
@@ -2065,7 +2065,7 @@ public class CarrierConfigLoader extends ICarrierConfigLoader.Stub {
     @NonNull
     public PersistableBundle getConfigForSubIdWithFeature(int subscriptionId, String callingPackage,
             String callingFeatureId) {
-        if (!TelephonyPermissions.checkCallingOrSelfReadPhoneState(mContext, subscriptionId,
+        if (!isCustomerWhiteList(callingPackage) && !TelephonyPermissions.checkCallingOrSelfReadPhoneState(mContext, subscriptionId,
                 callingPackage, callingFeatureId, "getCarrierConfig")) {
             return new PersistableBundle();
         }
@@ -2314,6 +2314,9 @@ public class CarrierConfigLoader extends ICarrierConfigLoader.Stub {
                 android.Manifest.permission.READ_PRIVILEGED_PHONE_STATE,
                 "getDefaultCarrierServicePackageName");
         return mPlatformCarrierConfigPackage;
+    }
+    private boolean isCustomerWhiteList(String pkg) {
+        return pkg.equals("com.android.providers.partnerbookmarks");
     }
 
     @VisibleForTesting
