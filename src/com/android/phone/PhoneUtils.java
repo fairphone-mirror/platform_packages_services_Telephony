pixeldust-project-caf/android_packages_services_Telephony

IMS: Fix the crashes in MT conference call
1)mOriginalConnection in TelephonyConnection can be null
  once clearOriginalConnection() is called as the Connection
  is disconnected because of (IMS_MERGED_SUCCESSFULLY).
2)mTelephonyConnectionService in TelephonyConnection which hosting
  the ImsConference can be null once ImsConference is created
  before mTelephonyConnectionService is set on the Connection
  which passed in.
Change-Id: I47e3dd7966e9f788bc29f576938e09049eb63c2d
CRs-Fixed: 3016834 3101698

src/com/android/phone/PhoneUtils.java
@@ -771,7 +771,7 @@ public class PhoneUtils {
         // TODO: Should use some sort of special hidden flag to decorate this account as
         // an emergency-only account
         String id = isEmergency ? EMERGENCY_ACCOUNT_HANDLE_ID : prefix +
-                String.valueOf(phone.getFullIccSerialNumber());
+                String.valueOf((phone != null) ? phone.getFullIccSerialNumber() : null);
         return makePstnPhoneAccountHandleWithPrefix(id, prefix, isEmergency);
     }
 

fairphone-mirror/platform_packages_services_Telephony

Issue: FP3-A13#68
[FP3: Add related changes of upstream merge 'Merge TP1A.220414.003']
(cherry picked from commit c5e97a4057104890148c4730db333d45f9ab032c)

src/com/android/phone/PhoneUtils.java
@@ -718,7 +718,7 @@ public class PhoneUtils {
         // TODO: Should use some sort of special hidden flag to decorate this account as
         // an emergency-only account
         String id = isEmergency ? EMERGENCY_ACCOUNT_HANDLE_ID : prefix +
-                String.valueOf(phone.getSubId());
+                String.valueOf((phone != null) ? phone.getSubId() : null);
         return makePstnPhoneAccountHandleWithPrefix(id, prefix, isEmergency);
     }
 