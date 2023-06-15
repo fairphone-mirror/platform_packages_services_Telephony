1d14b5ac64b873ce837ac7cc0e6b91b91b8a858d
xiaoyang.wang <xiaoyang.wang@t2mobile.com>
2023-06-15 13:09:11 +0800

    FP5-1788 Remove the redundant call settings from the menu
    * Root Cause
      Remove unused (and confusing) settings under Calling accounts as below:Smart-Divert
    * Solution
      Remove unused (and confusing) settings under Calling accounts as below:Smart-Divert
    * Test Steps
      1.Open PhoneCall->Settings->Calling accounts
      2.Smart-Divert
    
    Change-Id: Ie7bc6dbe0eee9f1cb087a356bed7b86b3d034c62

--- src/com/android/phone/CallFeaturesSetting.java
+++ src/com/android/phone/CallFeaturesSetting.java
@@ -427,6 +427,7 @@ public class CallFeaturesSetting extends PreferenceActivity
         Preference fdnButton = prefSet.findPreference(BUTTON_FDN_KEY);
         fdnButton.setIntent(mSubscriptionInfoHelper.getIntent(FdnSetting.class));
         if (carrierConfig.getBoolean(CarrierConfigManager.KEY_WORLD_PHONE_BOOL)) {
+            prefSet.removePreference(cdmaOptions);
             if (carrierConfig.getBoolean("config_common_callsettings_support_bool")) {
                 prefSet.removePreference(cdmaOptions);
                 prefSet.removePreference(gsmOptions);
@@ -560,6 +561,7 @@ public class CallFeaturesSetting extends PreferenceActivity
                 PhoneGlobals.getInstance().getCarrierConfigForSubId(mPhone.getSubId());
 
         mImsSettingsScreen = (PreferenceScreen) findPreference(BUTTON_IMS_SETTINGS_KEY);
+
       /*if (isImsSettingsApkAvailable(mPhone.getContext())) {
             ImsManager imsManager = ImsManager.getInstance(mPhone.getContext(),
                     mPhone.getPhoneId());
--- src/com/android/phone/settings/PhoneAccountSettingsFragment.java
+++ src/com/android/phone/settings/PhoneAccountSettingsFragment.java
@@ -84,7 +84,7 @@ public class PhoneAccountSettingsFragment extends PreferenceFragment
 
     private AccountSelectionPreference mDefaultOutgoingAccount;
     private Preference mAllCallingAccounts;
-    private Preference mSmartDivertPref;
+    // private Preference mSmartDivertPref;
 
     private PreferenceCategory mMakeAndReceiveCallsCategory;
     private SwitchPreference mButtonVibratingForMoCallAccepted;
@@ -160,7 +160,7 @@ public class PhoneAccountSettingsFragment extends PreferenceFragment
         mDefaultOutgoingAccount = (AccountSelectionPreference)
                 getPreferenceScreen().findPreference(DEFAULT_OUTGOING_ACCOUNT_KEY);
         mAllCallingAccounts = getPreferenceScreen().findPreference(ALL_CALLING_ACCOUNTS_KEY);
-        mSmartDivertPref = getPreferenceScreen().findPreference(BUTTON_SMART_DIVERT_KEY);
+        // mSmartDivertPref = getPreferenceScreen().findPreference(BUTTON_SMART_DIVERT_KEY);
 
         mMakeAndReceiveCallsCategory = (PreferenceCategory) getPreferenceScreen().findPreference(
                 MAKE_AND_RECEIVE_CALLS_CATEGORY_KEY);
@@ -391,12 +391,12 @@ public class PhoneAccountSettingsFragment extends PreferenceFragment
                 mMakeAndReceiveCallsCategoryPresent = false;
             }
 
-            if (isXdivertAvailable) {
-                if (mSmartDivertPref != null) {
-                    Log.d(LOG_TAG, "Add smart divert preference");
-                    mAccountList.addPreference(mSmartDivertPref);
-                }
-            }
+            // if (isXdivertAvailable) {
+            //     if (mSmartDivertPref != null) {
+            //         Log.d(LOG_TAG, "Add smart divert preference");
+            //         mAccountList.addPreference(mSmartDivertPref);
+            //     }
+            // }
         }
     }
 
