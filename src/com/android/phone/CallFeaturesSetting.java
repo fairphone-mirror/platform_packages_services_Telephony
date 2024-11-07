56de416343c2a622f1f0848b230004f589189971
renjiezhang <renjiezhang@t2mobile.com>
2024-11-07 17:34:21 +0800

    FP5V-268 [FP5V]Beta Testers: More untranslated text in Network page while phone is in Spanish
    * Root Cause
      Customer's requestment
    * Solution
      Fix code to meet customer' requestment
    * Test Steps
      Please refer to this Jira's Description -> Reproduce steps.
    
    Change-Id: I91c5469cfe79e6af8afdb83e1b72745aee6a991c

--- src/com/android/phone/CallFeaturesSetting.java
+++ src/com/android/phone/CallFeaturesSetting.java
@@ -610,7 +610,10 @@ public class CallFeaturesSetting extends PreferenceActivity
         } else if (!mImsMgr.isWfcEnabledByPlatform() || !mImsMgr.isWfcProvisionedOnDevice()) {
             prefSet.removePreference(mButtonWifiCalling);
         } else {
-            String title = getResourcesForSubId().getString(R.string.wifi_calling);
+            String title = carrierConfig.getString(CarrierConfigManager.KEY_WIFI_CALLING_TITLE);/* add by T2M.zhangrenjie for FP5-1630 */
+            if ("".equals(title)) {
+                title = getResourcesForSubId().getString(R.string.wifi_calling_settings_title);
+            }
             mButtonWifiCalling.setTitle(title);
 
             int resId = com.android.internal.R.string.wifi_calling_off_summary;
