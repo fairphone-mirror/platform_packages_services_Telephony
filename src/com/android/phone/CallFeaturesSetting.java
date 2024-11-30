8359da287cbbf848a6929b4938b15fe1501db95a
renjiezhang <renjiezhang@t2mobile.com>
2024-11-30 19:43:48 +0800

    FPS-251 [FPS]Beta Testers: More untranslated text in Network page while phone is in Spanish
    * Root Cause
      Customer's requestment
    * Solution
      Fix code to meet customer' requestment
    * Test Steps
      Please refer to this Jira's Description -> Reproduce steps.
    
    Change-Id: I71b25643b4c352b9e24171186d48029f8ec943d4

--- src/com/android/phone/CallFeaturesSetting.java
+++ src/com/android/phone/CallFeaturesSetting.java
@@ -437,6 +437,7 @@ public class CallFeaturesSetting extends PreferenceActivity
         Preference fdnButton = prefSet.findPreference(BUTTON_FDN_KEY);
         fdnButton.setIntent(mSubscriptionInfoHelper.getIntent(FdnSetting.class));
         if (carrierConfig.getBoolean(CarrierConfigManager.KEY_WORLD_PHONE_BOOL)) {
+            prefSet.removePreference(cdmaOptions);
             if (carrierConfig.getBoolean("config_common_callsettings_support_bool")) {
                 prefSet.removePreference(cdmaOptions);
                 prefSet.removePreference(gsmOptions);
@@ -610,7 +611,10 @@ public class CallFeaturesSetting extends PreferenceActivity
         } else if (!mImsMgr.isWfcEnabledByPlatform() || !mImsMgr.isWfcProvisionedOnDevice()) {
             prefSet.removePreference(mButtonWifiCalling);
         } else {
-            String title = getResourcesForSubId().getString(R.string.wifi_calling);
+            String title = carrierConfig.getString(CarrierConfigManager.KEY_WIFI_CALLING_TITLE);/* add by T2M.zhangrenjie for FPS-251 */
+            if ("".equals(title)) {
+                title = getResourcesForSubId().getString(R.string.wifi_calling_settings_title);
+            }
             mButtonWifiCalling.setTitle(title);
 
             int resId = com.android.internal.R.string.wifi_calling_off_summary;
@@ -635,7 +639,8 @@ public class CallFeaturesSetting extends PreferenceActivity
                         if (DBG) log("Unexpected WFC mode value: " + wfcMode);
                 }
             }
-            mButtonWifiCalling.setSummary(getResourcesForSubId().getString(resId));
+            boolean showSummary = carrierConfig.getBoolean("show_wifi_calling_summary_bool",true);/* add by T2M.zhangrenjie for FPS-251 */
+            if(showSummary) mButtonWifiCalling.setSummary(getResourcesForSubId().getString(resId));
             Intent intent = mButtonWifiCalling.getIntent();
             if (intent != null) {
                 intent.putExtra(Settings.EXTRA_SUB_ID, mPhone.getSubId());
