53e75dd4bdd88c0ce3a6cd9ebb1b9572c78bd79a
renjiezhang <renjiezhang@t2mobile.com>
2024-11-30 23:14:05 +0800

    FPS-248 [FPS][VoWiFi_Services_LowRisk][59x.1.2.1.a1 Mobile Originated Voice over IMS (VoWifi) call - Calling party or Called party hangs up] DUT is EVS capable but HD+ icon is not displayed on DUT
    * Root Cause
      Customer's requestment
    * Solution
      Fix code to meet customer' requestment
    * Test Steps
      Please refer to this Jira's Description -> Reproduce steps.
    
    Change-Id: Id43114f0fda8485ee63ce6fc75a7b1d1ac38be53

--- src/com/android/services/telephony/TelephonyConnection.java
+++ src/com/android/services/telephony/TelephonyConnection.java
@@ -1648,6 +1648,7 @@ abstract class TelephonyConnection extends Connection implements Holdable,
      */
     protected final void updateConnectionProperties() {
         int newProperties = buildConnectionProperties();
+        Log.i(this, "updateConnectionProperties: hasHighDefAudioProperty = " + hasHighDefAudioProperty());
 
         newProperties = changeBitmask(newProperties, PROPERTY_HIGH_DEF_AUDIO,
                 hasHighDefAudioProperty());
@@ -3141,6 +3142,7 @@ abstract class TelephonyConnection extends Connection implements Holdable,
     public void setAudioQuality(int audioQuality) {
         mHasHighDefAudio = audioQuality ==
                 com.android.internal.telephony.Connection.AUDIO_QUALITY_HIGH_DEFINITION;
+         Log.i(this, "setAudioQuality: mHasHighDefAudio = " + mHasHighDefAudio);
         updateConnectionProperties();
     }
 
